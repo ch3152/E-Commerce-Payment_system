2125e27b714722ca3092bdd482b3577a260b9e7e
ch3152 <148946524+ch3152@users.noreply.github.com>
2025-01-25 03:12:44 +0900

    Delete src directory

--- src/main/java/com/example/demo/repository/TossPaymentInfoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.TossPaymentInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TossPaymentInfoRepository extends JpaRepository<TossPaymentInfo, Long> {
-    // 추가적인 쿼리 메서드를 작성할 수 있습니다.
-}
